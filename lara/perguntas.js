651435dd7c7ac237b04131426c887fcb8abf37c9
Lara-rm <00001136400540sp@al.educacao.sp.gov.br>
2024-10-22 09:33:55 -0300

    Add files via upload

---
+++ lara/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Português',
+    'O que é uma sílaba tônica?',
+    'É a sílaba mais forte de uma palavra, sendo pronunciada com mais intensidade e força.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Alemanha?',
+    'A capital da Alemanha é Berlim.'
+)
+
+criaCartao(
+    'História',
+    'Quando se iniciou a colonização no Brasil?',
+    '1500 pelos portugueses.'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz sala de aula em Inglês?',
+    'Classroom (clesrrum).'
+)
